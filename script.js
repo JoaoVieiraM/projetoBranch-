43b56b22c647a5c3ea215ff242734207c88180f4
Joao <jpvieira1312@gmail.com>
2024-10-03 11:24:22 -0300

    função em javasc

--- script.js
+++ script.js
@@ -7,6 +7,26 @@ for(let i = 0; i <= 10;i++){
 
 let f = 0 
 while (f < 10 ){
-    console.log(f)
+    // console.log(f)
     f++;
 }
+//  do while 
+let w = 0 
+
+do{
+    // console.log(w)
+    w++
+}
+while(w < 5);
+
+// funções 
+
+function ola(nome){
+    // ``( strings literal ou template strings ${} chama a var)
+    // console.log(`seja bem vindo a ${nome}`)
+    alert(`seja bem vindo a ${nome}`)
+    document.write(`seja bem vindo a ${nome}`)
+
+}
+// executando a função ola passando valor 
+ola("fiap")
